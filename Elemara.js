9d87a2eff7d0082ab92cd2312b9d6708f1f134ef
NojaSuperJuice <janofodulio@gmail.com>
2024-12-07 15:50:56 -0500

    modified:   Elemara.js 	deleted:    commands/utility/server.js 	deleted:    commands/utility/user.js 	deleted:    deploy-commands.js 	deleted:    events/interactionCreate.js 	deleted:    events/ready.js

--- Elemara.js
+++ Elemara.js
@@ -1,30 +1,11 @@
 require('dotenv').config();
-
 const { token } = process.env.DISCORD_TOKEN
 
-const fs = require('node:fs');
-const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
+client.once('ready', () => {
+	console.log(`Ready! Logged in as ${client.user.tag}!.`);
+})
 
 client.login(token);
