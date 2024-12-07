90657bb2273a9da417160593037a0335b8dabd37
NojaSuperJuice <janofodulio@gmail.com>
2024-12-07 16:06:55 -0500

    modified:   Elemara.js

--- Elemara.js
+++ Elemara.js
@@ -8,4 +8,17 @@ client.once('ready', () => {
 	console.log(`Ready! Logged in as ${client.user.tag}!.`);
 })
 
+client.on('messageCreate', message => {
+	 if (message.content.startsWith('!settimer ')) { 
+		const args = message.content.split(' '); 
+		const time = parseInt(args[1]); if (isNaN(time)) { 
+			message.channel.send('Hey! You need to provide a valid number of seconds!'); 
+			return;
+		 } 
+		 
+		 message.channel.send(`Hey ${message.author.displayName}! You've set a timer set for ${time} seconds!`);
+		
+		 setTimeout(() => { message.channel.send(`Time's up ${message.author.displayName}!`); }, time * 1000); 
+		}
+	});
 client.login(token);
